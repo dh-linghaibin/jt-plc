abbd39bb813f8eea3cb785493d673b0a17e006c8
Steven <a673576000@qq.com>
2018-01-01 17:24:23 +0800

    不知道。。。 先缓存

---
+++ code/zmms_gd32/frame/include/mcu_lua.h
@@ -0,0 +1,19 @@
+/*
+ * This file is part of the 
+ *
+ * Copyright (c) 2016-2017 linghaibin
+ *
+ */
+
+#ifndef _MCU_LUA_H_
+#define _MCU_LUA_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
