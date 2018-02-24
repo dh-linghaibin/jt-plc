7f5b794aa3d8a022cd296fe853873f7080da063d
Steven <a673576000@qq.com>
2018-02-24 18:07:54 +0800

    配置解構 搭建

---
+++ code/zmms_gd32/frame/co_configure.c
@@ -0,0 +1,10 @@
+/*
+ * This file is part of the 
+ *
+ * Copyright (c) 2016-2018 linghaibin
+ *
+ */
+
+#include "co_configure.h"
+
+
---
+++ code/zmms_gd32/frame/include/co_configure.h
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the 
+ *
+ * Copyright (c) 2016-2010 linghaibin
+ *
+ */
+
+#ifndef _CO_CONFIGURE_H_
+#define _CO_CONFIGURE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32f10x.h"
+
+typedef struct _co_configure_time {
+	uint16_t open_light_time1;
+	uint16_t close_light_time1;
+} co_configure_time;
+
+typedef struct _co_configure_group {
+	co_configure_time time[3]; /* 3组时间 */
+	uint8_t priority : 4; /* 优先级 */
+	uint16_t start_time; /* 开始延时值 */
+	uint16_t centre_time; /* 中间延时值 */
+	uint8_t light_sen_open;
+	uint8_t light_sen_close;
+	uint16_t coil_adr[32];/*32 组回路*/
+} co_configure_group;
+
+typedef struct _co_configure_mode {
+	uint8_t mode;
+	co_configure_group manual[3];
+	co_configure_group time_control[3];
+	co_configure_group light_control[3];
+}co_configure_mode;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
